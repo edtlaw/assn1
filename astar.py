7baa369465dd48c1acbc6f0c930a07a5b2794427
noahturbs <36610564+noahturbs@users.noreply.github.com>
2018-02-26 02:55:17 -0500

    printtoody now accepts 2 parameters, does hackjob of printing to commandline

--- astar.py
+++ astar.py
@@ -1,6 +1,8 @@
 import heapq
 import sys
 import math
+import string
+
 #print strings - poo(str(int)), didn't test any other data types
 def poo(message):
     sys.stdout.write(message)
@@ -37,55 +39,50 @@ class Cell(object):
 # 0  0  0  0  0
 # 0  0  0  0  0
 
+for var in list(range(50)):              #delete # to go through all 50
+    input("Press Enter to continue")         #waits for user to press enter
+    fp = open(str(var) + '.txt', 'r') #var for 0
+    s = fp.read()
+    q=s.split()
+    #q is a list of numbers, every two numbers are coordinates for where the blocks are.
 
-#fp = open(str(var) + '.txt', 'r')
-#s = fp.read()
-#fp.close()
-maxRows=5 #101
-maxCols=5 #101
-toody=[[Cell(x,y,0) for y in range(maxCols)] for x in range(maxRows)]
 
+    fp.close()
+    maxRows=101 #101
+    maxCols=101 #101
+    toody=[[Cell(x,y,0) for y in range(maxCols)] for x in range(maxRows)]
 
-#while
-
-toody[0][1].blocked=1
-toody[2][1].blocked=1
-toody[0][3].blocked=1
-toody[1][3].blocked=1
-start = toody[0][0]
-goal = toody[1][4]
-start.g=0
-start.prevCell=None
+    qiter = iter(q)
+    for numbers in qiter:
+        toody[int(numbers)][int(next(qiter))].blocked=1
 
 
 
-#    fp.close()
+    #toody[0][1].blocked=1
+    #toody[2][1].blocked=1
+    #toody[0][3].blocked=1
+    #toody[1][3].blocked=1
 
 
-def printToody(toody,closedList):
-    for x in range(maxRows):
-        for y in range(maxCols):
-            temp=' '
-            if toody[x][y].blocked==1:
-                poo('B'+temp)
-            elif toody[x][y].blocked==0:
-                if toody[x][y] in closedList:
-                    poo('P'+temp)
-                else:
-                    poo('O'+temp)
-        poo("\n")
+    toody[5][5].blocked = 0         #we should keep the goals and starts constant, idk why I made end.txts >_>
+    toody[20][20].blocked = 0
+    start = toody[5][5]
+    goal = toody[20][20]
+    start.g=0
+    start.prevCell=None
 
-
-
-#def printToody1(toody,closedList,currCell):
-#    for x in range(maxRows):
-            #if toody[x][y].blocked==1:
-    #            poo('B'+temp)
-    #            if toody[x][y] in
-    #            if toody[x][y] in closedList:
-    #            else:
-    #                poo('O'+temp)
-    #    poo("\n")
+    def printToody(toody,closedList):   #doesn't print the "finished" iteration of the map. looks incomplete.
+        for x in range(33):          #prints too much orginally maxRows
+            for y in range(33):      #prints too much holy god maxCols
+                temp=' '
+                if toody[x][y].blocked==1:
+                    poo('B'+temp)
+                elif toody[x][y].blocked==0:
+                    if toody[x][y] in closedList:
+                        poo('P'+temp)
+                    else:
+                        poo('O'+temp)
+            poo("\n")
 
 
 
@@ -95,75 +92,78 @@ def printToody(toody,closedList):
 
 
 
-openList = []
-heapq.heappush(openList,(start.f,start))
-closedList = []
 
-#backtrack from goal to start
-def reconstructPath(curCell):
-    poo(str(curCell.x) + ', ' + str(curCell.y) + '\n')
-    while curCell and curCell.prevCell is not None:
-        curCell=curCell.prevCell
+
+
+    openList = []
+    heapq.heappush(openList,(start.f,start))
+    closedList = []
+
+    #backtrack from goal to start
+    def reconstructPath(curCell):
         poo(str(curCell.x) + ', ' + str(curCell.y) + '\n')
+        while curCell and curCell.prevCell is not None:
+            curCell=curCell.prevCell
+            poo(str(curCell.x) + ', ' + str(curCell.y) + '\n')
 
-def findpath(curCell):
-    poo(str(curCell.x) + ', ' + str(curCell.y) + '\n')
-    while curCell and curCell.prevCell is not None:
-        curCell=curCell.prevCell
+    def findpath(curCell):
         poo(str(curCell.x) + ', ' + str(curCell.y) + '\n')
+        while curCell and curCell.prevCell is not None:
+            curCell=curCell.prevCell
+            poo(str(curCell.x) + ', ' + str(curCell.y) + '\n')
 
-def manhattanDistance(cur,goal):
-    return abs(cur.x-goal.x)+abs(cur.y-goal.y)
+    def manhattanDistance(cur,goal):
+        return abs(cur.x-goal.x)+abs(cur.y-goal.y)
 
-#grid, blooks, search
-def search(start,goal):
-    print('searching')
-    if not openList:
-        print('empty')
-    while openList:
-        printToody(toody,closedList)
-        print('getting')
-        curCell=heapq.heappop(openList)[1]
-        closedList.append(curCell)
-        if curCell is None:
-            return 'failure'
-        poo('curCell: '+str(curCell.x)+', '+str(curCell.y)+'\n')
-        if curCell == goal:
-            print('found')
-            return reconstructPath(curCell)
-        x=curCell.x
-        y=curCell.y
-        #check neighbors:
-        def checkNeighbor(x,y):
-            if x>maxRows-1 or y>maxCols-1 or x<0 or y<0:
-                return
-            nextCell = toody[x][y]
-            if nextCell in closedList or nextCell.blocked:
-                return
-            #actions always cost 1 unit for this grid
-            tempG=curCell.g + 1
-            poo('nextCell g: ' + str(nextCell.g) + 'curCell g: ' +str(curCell.g)+'\n')
-            if curCell.g+1<nextCell.g:
-                print('found better path')
-                nextCell.prevCell=curCell
-                nextCell.g=tempG
-                nextCell.f=tempG+manhattanDistance(nextCell,goal)
-                poo('nextCell: ' + str(nextCell.x) + ', ' + str(nextCell.y) + '\n')
-            if (nextCell.f,nextCell) not in openList:
-                heapq.heappush(openList,(nextCell.f,nextCell))
+    #grid, blooks, search
+    def search(start,goal):
+        print('searching')
+        if not openList:
+            print('empty')
+        while openList:
+            printToody(toody,closedList)
+            print('getting')
+            curCell=heapq.heappop(openList)[1]
+            closedList.append(curCell)
+            if curCell is None:
+                return 'failure'
+            poo('curCell: '+str(curCell.x)+', '+str(curCell.y)+'\n')
+            if curCell == goal:
+                print('found')
+                return reconstructPath(curCell)
+            x=curCell.x
+            y=curCell.y
+            #check neighbors:
+            def checkNeighbor(x,y):
+                if x>maxRows-1 or y>maxCols-1 or x<0 or y<0:
+                    return
+                nextCell = toody[x][y]
+                if nextCell in closedList or nextCell.blocked:
+                    return
+                #actions always cost 1 unit for this grid
+                tempG=curCell.g + 1
+                poo('nextCell g: ' + str(nextCell.g) + 'curCell g: ' +str(curCell.g)+'\n')
+                if curCell.g+1<nextCell.g:
+                    print('found better path')
+                    nextCell.prevCell=curCell
+                    nextCell.g=tempG
+                    nextCell.f=tempG+manhattanDistance(nextCell,goal)
+                    poo('nextCell: ' + str(nextCell.x) + ', ' + str(nextCell.y) + '\n')
+                if (nextCell.f,nextCell) not in openList:
+                    heapq.heappush(openList,(nextCell.f,nextCell))
 
-        #up,down,left,right
-        checkNeighbor(x-1, y)
-        checkNeighbor(x+1,y)
-        checkNeighbor(x, y - 1)
-        checkNeighbor(x,y+1)
+            #up,down,left,right
+            checkNeighbor(x-1, y)
+            checkNeighbor(x+1,y)
+            checkNeighbor(x, y - 1)
+            checkNeighbor(x,y+1)
+        print('not found')
+        return 'not found'
+    search(start,goal)
 
-    return 'not found'
-search(start,goal)
+    #search
+    #check in order of up right left down for each action.
+    #calculate cost by f=g+h, where g is the actual cost accumulated up to this point, h is absolute x,y of point - goal.
 
-#search
-#check in order of up right left down for each action.
-#calculate cost by f=g+h, where g is the actual cost accumulated up to this point, h is absolute x,y of point - goal.
-
-#sort by priority queue for now, implement binary heap later when finished
-#take lowest cost, repeat
+    #sort by priority queue for now, implement binary heap later when finished
+    #take lowest cost, repeat
