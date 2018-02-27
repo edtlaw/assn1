8b8feb4e3991536280f508cd659aba3c325cfd68
Edmund Law <etl29@scarletmail.rutgers.edu>
2018-02-26 23:04:57 -0500

    pickle files

--- test.py
+++ test.py
@@ -4,18 +4,13 @@ import pickle
 import sys
 
 def main():
-    sys.setrecursionlimit(14000)
-    gw = GridWorld(10)
-    gw.buildMaze()
-    for i in range(len(gw.grid)):
-        for j in range(len(gw.grid[i])):
-            print(gw.grid[i][j].blocked, " ", end ="")
-        print('\n')
-    print('\n')
-    with open('graph_1.pkl', 'wb') as output:
-        pickle.dump(gw, output, pickle.HIGHEST_PROTOCOL)
-    del gw
-    with open('graph_1.pkl', 'rb') as input:
+    sys.setrecursionlimit(20000)
+
+
+
+
+
+    with open('graph_21.pkl', 'rb') as input:
         gw = pickle.load(input)
         for i in range(len(gw.grid)):
             for j in range(len(gw.grid[i])):
